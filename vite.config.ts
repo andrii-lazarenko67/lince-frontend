737dfc57dfaed6d8a3c135266852639bf5606f44
andrii-lazarenko67 <borysenkooleh835@gmail.com>
2025-12-02 06:31:48 -0500

    fixed vite configuration file

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,8 @@ export default defineConfig({
   preview: {
     host: '0.0.0.0',
     port: Number(process.env.PORT) || 4173,
-    allowedHosts: [process.env.VITE_PREVIEW]
+    strictPort: true,
+    allowedHosts: ['.railway.app']
   },
   build: {
     outDir: 'dist',
